543a8e60a28f0146bb6eceb0fc0e6d3bfdf9c852
Seyit <seyit@howlet.io>
2025-05-31 09:53:20 +0300

    swagger ui initialized

--- src/main/java/com/healthfintel/backend/BackendApplication.java
+++ src/main/java/com/healthfintel/backend/BackendApplication.java
@@ -1,8 +1,17 @@
 package com.healthfintel.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "HeAlthFintel Backend With Spring OpenAPI",
+				version = "1.0",
+				description = "API documentation for HeAlthFintel Backend"
+		)
+)
 @SpringBootApplication
 public class BackendApplication {
 
--- src/main/java/com/healthfintel/backend/controller/InsuranceController.java
+++ src/main/java/com/healthfintel/backend/controller/InsuranceController.java
@@ -3,6 +3,8 @@ package com.healthfintel.backend.controller;
 
 import com.healthfintel.backend.dto.InsuranceHistoryResponseDto;
 import com.healthfintel.backend.service.InsuranceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "User Controller", description = "Operations related to Insurance")
 public class InsuranceController {
 
     @Autowired
     InsuranceService insuranceService;
 
     @GetMapping("/insurance-history")
+    @Operation(
+            summary = "Get Insurance History by ID",
+            description = "Returns an array of insurance history based on the provided user ID"
+    )
     public List<InsuranceHistoryResponseDto> InsuranceHistory(){
         return this.insuranceService.getInsuranceHistoryByUserId(1L);
     }
